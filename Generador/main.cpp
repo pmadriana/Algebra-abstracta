86ee632b3f8bd4571183267c2721aa8c910163c7
Adriana Perez <adriana.perez@ucsp.edu.pe>
2017-05-12 19:20:53 -0500

    Update main.cpp

--- Generador/main.cpp
+++ Generador/main.cpp
@@ -1,103 +1,123 @@
-#include <iostream>
-#include <vector>
-#include <NTL/ZZ.h>
-#include <stdlib.h>
+#include "Generador_Aleatorio.h"
 
-using namespace NTL;
-using namespace std;
-
-void corrimiento_derecha(vector <int> &vec, int indice, int elementos, int vueltas)
+void swap(int i, int j, vector<bool>a)
 {
-    for(int j = vueltas; j > 0; j--)
+    int temp= a[i];
+    a[i] = a[j];
+    a[j] =temp;
+}
+void corrimiento_derecha(vector <bool> vec, int indice, int elementos, int vueltas)
+{
+    srand(time(NULL));
+    int pos_aleatoria = rand()% elementos;
+    bool bit_ultimo= vec[(elementos*2)-1];
+    for(int j =vueltas; j > 0; j--)
     {
-        for(int i = elementos + indice - 1; i > indice; i--)
+        for(int i=(elementos*2)-1-indice ; i>= elementos; i--)
         {
-            int temp = vec[i];
-            vec[i] = vec[i - 1];
-            vec[i - 1] = temp;
+            swap(i, i+1, vec);
         }
     }
+    bool x = vec[pos_aleatoria+elementos]^bit_ultimo; //pasar a aleatorio pe
+    vec[elementos]= x;
+//    for(int i=elementos; i<(elementos*2);i++)
+//        cout<<vec[i];
 }
-void corrimiento_izquierda(vector <int> &vec, int indice, int elementos, int vueltas)
+
+void corrimiento_izquierda(vector <bool> vec, int indice, int elementos, int vueltas)
 {
+    srand(time(NULL));
+    int pos_aleatoria = rand()% (elementos-1);
+    bool primero = vec[0];
     for(int j = vueltas; j > 0; j--)
     {
-        for(int i = indice; i < indice + elementos - 1; i++)
+        for(int i = 0; i<elementos-1-indice; i++)
         {
-            int temp = vec[i];
-            vec[i] = vec[i + 1];
-            vec[i + 1] = temp;
+            swap(i, i+1, vec);
         }
     }
+    bool x= primero^vec[pos_aleatoria];
+    vec[elementos-1]=x;
 }
-ZZ convertir_decimal(vector <int> a, int bits_num)
+
+ZZ decimal(vector <bool> b, int bits_num)
 {
     ZZ num;
     num = 0;
     int e = 0;
     for(int i = bits_num - 1; i >= 0; i--)
     {
-        if(a[i]==1)
-        {
-            num += power2_ZZ(e);
-        }
+        if(b[i]==1)
+            (num += power2_ZZ(e));
         else
-            num += to_ZZ(0);
+            (num += to_ZZ(0));
         e += 1;
     }
     return num;
 }
-ZZ generador(long int t_seed, int t_bits, int particiones, int vueltas)
+int rdtsc() //rand de ciclos utilizados por procesador desde el inicio
 {
-    ZZ seed_aleatoria;
-    seed_aleatoria=RandomLen_ZZ(t_seed);
-//    cout<<seed_aleatoria<<endl;
-    vector <int>binario;
-    for(int i=t_seed-1; i>=0; i--){
-        binario.push_back(bit(seed_aleatoria, i));
+    __asm__ __volatile__("rdtsc");
+}
+ZZ generador(int t_seed, int t_bits, int particiones, int vueltas)
+{
+    vector <bool>binario;
+    bool aleatorio_bit_seed= 0;
+    for(int i = 0; i < t_bits; i++)
+    {
+        binario.push_back(0); //llenamos el vector
     }
-    int s=0;
+    int s=0;   //j pos vector ,s recorre
+    for(int i = 0 ; i<t_seed; i++)
+    {
+        srand(rdtsc());
+        aleatorio_bit_seed= rand()%2;
+        binario[i]= aleatorio_bit_seed;
+        if(binario[0]==0)
+            binario[0]=1;
+    }
+
     for(int i = t_seed; i < t_bits; i++)
     {
-        binario[i] = binario[s] ^ binario[s+1];
+        binario[i] = binario[s] ^ binario[s+1]; //concatenar xor
         s++;
     }
-    int bin;
-//    for(int i=0; i<t_bits; i++)
-//        cout<<binario[i];
 
-    int partes=t_bits/particiones;
-    int residuo_partes= t_bits%particiones;
-    if(residuo_partes!=0)
-    {
-        partes += 1;
-    }
-    int indice = 0;
+    int partes=t_bits/particiones; //cuantos bits tiene cada part
+    int residuo_partes= t_bits%particiones; //por si no es exacto
+
+    int indice=0;
     while(indice < t_bits)
     {
-        if((indice + partes) > t_bits)
+        if((indice + partes*2) > t_bits)
         {
             if(particiones%2!=0)
             {
                 corrimiento_izquierda(binario, indice, residuo_partes, vueltas);
                 indice += partes;
             }
-            else
+            else{
                 corrimiento_derecha(binario, indice, residuo_partes, vueltas);
                 indice += partes;
+            }
         }
         corrimiento_izquierda(binario, indice,partes, vueltas);
         indice += partes;
         corrimiento_derecha(binario, indice, partes, vueltas);
         indice += partes;
     }
+//     for(int i=0; i<binario.size(); i++)
+//    {
+//        cout<<binario[i];
+//    }
+//    cout<<endl;
     ZZ num;
-    num = convertir_decimal(binario, t_bits);
+    num = decimal(binario, t_bits);
     return num;
 
 }
 
 int main()
 {
-        cout<<generador(4,10,2,2);
+    cout<<generador(100,2048,2,5);
 }
