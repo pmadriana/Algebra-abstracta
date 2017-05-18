3842f0c90fa1a0fcf84618b19f571c2c63332e0d
Adriana Perez <adriana.perez@ucsp.edu.pe>
2017-05-18 16:14:16 -0500

    Update Generador_Aleatorio.h

--- RSA/Generador_Aleatorio.h
+++ RSA/Generador_Aleatorio.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <NTL/ZZ.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace NTL;
 using namespace std;
@@ -12,6 +13,8 @@ void swap(int , int , vector<bool> );
 void corrimiento_derecha(vector <bool>, int , int, int );
 void corrimiento_izquierda(vector <bool> , int , int , int );
 ZZ decimal(vector <bool> , int );
+int rdtsc();
 ZZ generador(int , int , int , int );
 
 #endif // GENERADOR_ALEATORIO_H
+
